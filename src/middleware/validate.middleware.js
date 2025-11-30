dc0e1318b9b0ffe1ea44bd76652bb3a477e1313b
Olatunde Sleek <olatundesleek@gmail.com>
2025-11-30 16:19:24 +0100

    updated release resllt endpoint

--- src/middleware/validate.middleware.js
+++ src/middleware/validate.middleware.js
@@ -20,3 +20,17 @@ export function validateParams(schema) {
     next();
   };
 }
+
+export function validateQuery(schema) {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req.query);
+    if (error)
+      return res.status(400).json({
+        success: false,
+        error: "Validation error",
+        details: error.details[0].message,
+      });
+    req.query = value;
+    next();
+  };
+}
