4ed0c0ff53e761fc6e86efdc56c947a51d9ecdad
Olatunde Sleek <olatundesleek@gmail.com>
2025-12-12 15:36:38 +0100

    fix for admin result not downloading

--- src/routes/result.routes.js
+++ src/routes/result.routes.js
@@ -52,7 +52,7 @@ router.get(
   "/download",
   authenticate,
   authorizeRoles("ADMIN", "TEACHER"),
-  validateParams(getAllResultsSchema),
+  validateQuery(getAllResultsSchema),
   resultController.downloadResults
 );
 
@@ -70,7 +70,7 @@ router.get(
   "/student/courses/download",
   authenticate,
   authorizeRoles("STUDENT"),
-
+  validateQuery(getStudentCourseResultsSchema),
   resultController.downloadStudentCourseResults
 );
 
